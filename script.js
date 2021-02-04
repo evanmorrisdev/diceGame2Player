da185c8886d8f8c6551890e4ac4acaabcaf0ab48
Evan Morris <evanmorris97@gmail.com>
2021-02-04 15:30:37 +0000

    html template development

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+//click new game
+//roll the dice
+//if score 1, your score resests
+//if you score anything else, it adds to your score.
+//player can then decide to hold the score or continue to roll
+//if hold, score is held and player 2 goes.
